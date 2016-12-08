d32e4e5a1022b0f028a57d957a92da274f2d9227
AuroraDoris <nguyenmtmcuong@gmail.com>
2016-12-08 16:44:12 +0700

    Tao class KhachHang,LoaiXe,NhanVien,PhuTung

---
+++ QuanLyCuaHangPhuTungXeMay/KhachHangClass.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCuaHangPhuTungXeMay
+{
+    class KhachHang
+    {
+        string ma, ten, maloaixe, bienso, ghichu;
+        int sdt, diemtichluy, solansuachua;
+
+        public string Ma
+        {
+            get { return ma; }
+            set { ma = value; }
+        }
+
+        public string Ten
+        {
+            get { return ten; }
+            set { ten = value; }
+        }
+
+        public int SDT
+        {
+            get { return sdt; }
+            set { sdt = value; }
+        }
+
+        public string MaLoaiXe
+        {
+            get { return maloaixe; }
+            set { maloaixe = value; }
+        }
+
+        public string BienSo
+        {
+            get { return bienso; }
+            set { bienso = value; }
+        }
+
+        public int SoLanSuaChua
+        {
+            get { return solansuachua; }
+            set { solansuachua = value; }
+        }
+
+        public int DiemTichLuy
+        {
+            get { return diemtichluy; }
+            set { diemtichluy = value; }
+        }
+
+        public string GhiChu
+        {
+            get { return ghichu; }
+            set { ghichu = value; }
+        }
+
+        public KhachHang() { }
+        public KhachHang(string ma, string ten, int sdt, string maloaixe, string bienso, int solansuachua, int diemtichluy, string ghichu)
+        {
+            this.ma = ma;
+            this.ten = ten;
+            this.sdt = sdt;
+            this.maloaixe = maloaixe;
+            this.bienso = bienso;
+            this.solansuachua = solansuachua;
+            this.diemtichluy = diemtichluy;
+            this.ghichu = ghichu;
+        }
+    }
+}
---
+++ QuanLyCuaHangPhuTungXeMay/PhuTungClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCuaHangPhuTungXeMay
+{
+    class PhuTung
+    {
+        string mapt, maloai, tenpt, donvi, ghichu;
+        int gianhap, giaban, slton;
+
+        public string MaPT
+        {
+            get { return mapt; }
+            set { mapt = value; }
+        }
+
+        public string MaLoai
+        {
+            get { return maloai; }
+            set { maloai = value; }
+        }
+
+        public string TenPT
+        {
+            get { return tenpt; }
+            set { tenpt = value; }
+        }
+
+        public string DonVi
+        {
+            get { return donvi; }
+            set { donvi = value; }
+        }
+
+        public string GhiChu
+        {
+            get { return ghichu; }
+            set { ghichu = value; }
+        }
+
+        public int GiaNhap
+        {
+            get { return gianhap; }
+            set { gianhap = value; }
+        }
+
+        public int GiaBan
+        {
+            get { return giaban; }
+            set { giaban = value; }
+        }
+
+        public int SLTon
+        {
+            get { return slton; }
+            set { slton = value; }
+        }
+
+        public PhuTung() { }
+        public PhuTung(string mapt, string maloai, string tenpt, string donvi, int gianhap, int giaban, int slton, string ghichu)
+        {
+            this.mapt = mapt;
+            this.maloai = maloai;
+            this.tenpt = tenpt;
+            this.donvi = donvi;
+            this.gianhap = gianhap;
+            this.giaban = giaban;
+            this.slton = slton;
+            this.ghichu = ghichu;
+        }
+    }
+}
